f6138ffff6e0e384811a7f927f6224897807da8f
Tofite <tofite76@gmail.com>
2019-12-29 00:04:42 +1100

    Fixed D3 Output

--- mach_learn.py
+++ mach_learn.py
@@ -4,8 +4,8 @@ from sklearn.linear_model import LinearRegression
 
 import matplotlib.pyplot as plt
 import datetime as dt
-
-
+import numpy
+import mpld3
 def add_years(d, years):
     """Return a date that's `years` years after the date (or datetime)
     object `d`. Return the same calendar date (month and day) in the
@@ -17,25 +17,20 @@ def add_years(d, years):
         return d.replace(year = d.year + years)
     except ValueError:
         return d + (dt.date(d.year + years, 1, 1) - dt.date(d.year, 1, 1))
-parser = Financials("AAPL")
-#col_series = parser.income_statement.columns
+parser = Financials("AMZN")
 stored = parser.income_statement
-#print(parser.income_statement)
-#income_points = parser.income_statement.loc["Net Income"]
 income_points = stored.loc["Net Income"]
-#test_corrected = parser.income_statement
 corrected_indices = income_points.index.map(dt.datetime.toordinal).to_numpy()
-#print(corrected_indices.reshape(1,-1))
 corrected_values = income_points.to_numpy()
-#print(corrected_values.reshape(1, -1))
 xTrain, xTest, yTrain, yTest = train_test_split(corrected_indices, corrected_values, test_size = 0.2)
-#print(yTrain)
 income_model = LinearRegression().fit(xTrain.reshape(-1, 1), yTrain)
-#value_at_2021 = add_years(dt.datetime.now(), 1)
-
-#a = xTest.to_numpy().reshape(1, -1)
 print(income_model.coef_)
 prediction = income_model.predict(xTest.reshape(-1,1))
 print(prediction)
-plt.scatter(xTest.tolist(), prediction.tolist())
-plt.show()
+print(xTest)
+#plottable_x = dt.datetime.fromordinal(xTest)
+plottable_x = numpy.vectorize(dt.datetime.fromordinal)(xTest)
+plt.scatter(plottable_x.tolist(), prediction.tolist())
+mpld3.show()
+print(mpld3.fig_to_html(plt.gcf()))
+#plt.show()
