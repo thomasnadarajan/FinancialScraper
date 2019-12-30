5d9f816ad2470ff4f6f80f6c8609a510a820b724
Tofite <tofite76@gmail.com>
2019-12-30 22:18:55 +1100

    updated price machine learning

--- mach_learn_price.py
+++ mach_learn_price.py
@@ -14,7 +14,6 @@ for dicter in corrected['historical']:
     corrected_d = dt.date(int(dicter['date'].split('-')[0]), int(dicter['date'].split('-')[1]), int(dicter['date'].split('-')[2])).toordinal()
     array_temp_time.append(corrected_d)
     array_temp_price.append(float(dicter['close']))
-print(array_temp_time)
 parser = Financials("AAPL")
 stored = parser.income_statement
 income_points = stored.loc["Net Income"]
@@ -25,17 +24,25 @@ for x in dates:
     while i < len(array_temp_time):
         if array_temp_time[i] == x:
             logged_indices.append(i)
-        elif array_temp_time > x:
+            break
+        elif array_temp_time[i] > x:
             logged_indices.append(i-1)
-        elif i == len(array_temp_time) - 1:
-            logged_indices.append(i)
+            break
+        i+=1
+#print(logged_indices)
+corrected = []
+for x in logged_indices:
+    i = 0
+    while i < len(array_temp_price):
+        if i == x:
+            corrected.append(array_temp_price[i])
+            break
         i+=1
 
-corrected = []
-while i < len(array_temp_price):
-    for x in logged_indices:
-        if x == i:
-            corrected.append(array_temp_price[i])
-    i+=1
-#while i < len()
-#xTrain, xTest, yTrain, yTest = train_test_split()
+#print(corrected)
+#print(income_points.values.tolist())
+#corrected_y = np.ndarray
+corrected.reverse()
+xTrain, xTest, yTrain, yTest = train_test_split(income_points.values, np.array(corrected), test_size=0.2)
+print(xTest, yTest)
+price_model = LinearRegression().fit(xTrain.reshape(-1, 1), yTrain.reshape(-1,1))
